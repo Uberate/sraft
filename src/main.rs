b8ce076c82017b626abb5d2d2e5f5701c0bbdccd
uberate <ubserate@gmail.com>
2022-12-11 15:09:33 +0800

    add plugin for storage

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-pub mod server;
-pub mod point;
-pub mod storage;
 pub mod config;
+pub mod point;
+pub mod plugins;
+pub mod server;
+pub mod storage;
+
 
 // == test mod
 #[cfg(test)]
 mod config_test;
+
 
 fn main() {
     println!("Hello, world!");
